ad7230d60cbbfb04d13524c8026317975936713e
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2024-10-20 08:34:55 +0000

    data retrieval and qa chain is completed

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -7,18 +8,19 @@ from langchain_community.document_loaders import ReadTheDocsLoader
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 
-embeddings = OpenAIEmbeddings(model = "text-embedding-3-small")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
 
 def ingest_docs():
     loader = ReadTheDocsLoader(
-            path="langchain-docs1/api.python.langchain.com/en/latest",
-            encoding="ISO-8859-1",
-        )
-    
+        path="langchain-docs1/api.python.langchain.com/en/latest",
+        encoding="ISO-8859-1",
+    )
+
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 600, chunk_overlap = 50)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
 
     for doc in documents:
@@ -29,10 +31,11 @@ def ingest_docs():
     print(f"Going to add {len(documents)} to Pinecone")
 
     PineconeVectorStore.from_documents(
-        documents, embeddings, index_name = os.environ["INDEX_NAME"]
+        documents, embeddings, index_name=os.environ["INDEX_NAME"]
     )
 
     print("****Loading to vectorstore done****")
+
 
 if __name__ == "__main__":
     ingest_docs()
